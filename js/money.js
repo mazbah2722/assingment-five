b0f8935d6e4bfc5faf3475cf66cd0561290005d5
mazbah2722 <mazbahuddin82269@gmail.com>
2024-12-07 21:50:41 +0600

    alert added

--- js/money.js
+++ js/money.js
@@ -6,28 +6,51 @@ document.getElementById('donate-btn').addEventListener('click', function(event){
 
    const addMoney = getInputFieldValueById('donate-input');
    const balance = getTextfieldValueById('initial-value');
+   
+   
+   if(isNaN(addMoney)){
+    alert('Failed');
+    return 0;
+   }
    const newBalance = balance + addMoney;
    document.getElementById('initial-value').innerText = newBalance;
+//    
+   const myBalance = document.getElementById('myBalance').innerText;
+   const myBalanceConvert = parseFloat(myBalance);
+   const newMyBalance = myBalanceConvert - newBalance;
+   document.getElementById('myBalance').innerText = newMyBalance;
+
+
   
    console.log(addMoney, balance, newBalance);
-   
+   alert('successfully');
 
 })
 document.getElementById('donate2-btn').addEventListener('click', function(event){
     event.preventDefault();
    const addMoney2 = getInputFieldValueById('feni-input');
    const balance = getTextfieldValueById('initial-value2');
+   if(isNaN(addMoney2)){
+    alert('Failed');
+    return 0;
+   }
    const newBalance = balance + addMoney2;
    document.getElementById('initial-value2').innerText = newBalance;
    console.log( addMoney2);
+   alert('successfully');
 
 })
 document.getElementById('quota-protest-btn').addEventListener('click', function(event){
     event.preventDefault();
    const addMoney3 = getInputFieldValueById('quota-protest-input');
    const balance = getTextfieldValueById('initial-value3');
+   if(isNaN(addMoney3)){
+    alert('Failed');
+    return 0;
+   }
    const newBalance = balance + addMoney3;
    document.getElementById('initial-value3').innerText = newBalance;
    console.log( addMoney3);
+   alert('successfully');
 
 })
